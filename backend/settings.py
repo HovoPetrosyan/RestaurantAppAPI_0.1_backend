27e3b842185da04afa1d6f42b6d7ba082cb95e78
Hovo_Petrosyan <51003609+HovoPetrosyan@users.noreply.github.com>
2021-12-10 16:02:31 +0400

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -116,7 +116,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'frontend/build')],
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
